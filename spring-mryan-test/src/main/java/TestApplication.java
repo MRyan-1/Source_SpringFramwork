388dce552354570b4b15da86a15ea405f6ba56fd
MRyan <770255975@qq.com>
2021-09-01 11:37:33 +0800

    checkout springFramework 5.1x and fix [other people import error]

---
+++ spring-mryan-test/src/main/java/TestApplication.java
@@ -0,0 +1,14 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author MRyan
+ * @version 1.0.0
+ * @date 2021/08/30 23:54
+ */
+public class TestApplication {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+		String name = applicationContext.getApplicationName();
+		System.out.println("name = " + name);
+	}
+}
